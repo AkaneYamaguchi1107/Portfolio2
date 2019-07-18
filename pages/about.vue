d6ae90c548aeea3d534b4dfd37fd17006c353dad
user <user@user-no-MacBook-Pro.local>
2019-07-18 15:31:31 +0900

    リポジトリ作成、連携、環境構築、scss

---
+++ pages/about.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>About</h1>
+    <p>アバウトページです</p>
+    <nuxt-link to="/">Topへ</nuxt-link>
+  </div>
+</template>
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,35 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        Dog
-      </h1>
-      <h2 class="subtitle">
-        My slick Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div>
+    <div class="hero">
+      <h1 class="hero__title">はじめてのNuxt.js</h1>
+      <p class="hero__sub-title">はじめてです。</p>
+      <div class="hero__button">
+        <button class="btn">
+          <nuxt-link to="about">アバウトページへ</nuxt-link>
+        </button>
       </div>
     </div>
   </div>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
-</script>
-
-<style scoped>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
+<style lang="scss" scoped>
+.hero {
+  padding: 0 16px;
+  height: 600px;
+  background: #000;
   display: flex;
+  flex-flow: column;
   justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  color: #fff;
+  background: center/cover url(/images/hero.jpg);
+  &__title {
+    font-size: 32px;
+    font-weight: bold;
+    margin-bottom: 24px;
+  }
+  &__sub-title {
+    font-size: 24px;
+    margin-bottom: 24px;
+  }
 }
 </style>
